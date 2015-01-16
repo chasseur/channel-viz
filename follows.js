b0bd38d09f950bd9b04ed9e5a7e9b8dd3ccb1ce7
chasseur <allenhm@gmail.com>
2015-01-15 18:55:25 -0600

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= '380p3cxLhGBH2Thg1H7doL2zcaoQLCDovFav2C43epC1UMvx', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [69373229], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'MyStall Development', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
